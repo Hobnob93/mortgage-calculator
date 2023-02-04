00f5595ba78f7e0ce30ebd248005cf33bc4ac04f
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2023-02-04 16:18:11 +0000

    Added mortgage document model

---
+++ MortgageCalculator/MortgageCalculator.Core/Models/Mortgage.cs
@@ -0,0 +1,16 @@
+﻿namespace MortgageCalculator.Core.Models;
+
+public record Mortgage : DocumentBase
+{
+    public string Name { get; set; } = string.Empty;
+    public string Provider { get; set; } = string.Empty;
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
+    public DateOnly FixedTermEndDate { get; set; }
+    public int FullTermLength { get; set; }
+    public int FixedTermLength { get; set; }
+    public bool HasRemortgaged { get; set; }
+    public double InterestRate { get; set; }
+    public decimal MonthlyPayment { get; set; }
+    public House? House { get; set; }
+}

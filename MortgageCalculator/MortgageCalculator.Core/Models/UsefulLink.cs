09ad9601336d173991fc4d73a2bd35282d3959c9
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2023-02-07 21:47:45 +0000

    Added Icon Name property to UsefulLink data

--- MortgageCalculator/MortgageCalculator.Core/Models/UsefulLink.cs
+++ MortgageCalculator/MortgageCalculator.Core/Models/UsefulLink.cs
@@ -3,6 +3,7 @@
 public record UsefulLink : DocumentBase
 {
     public required string Name { get; init; }
+    public required string IconName { get; init; }
     public required string Icon { get; init; }
     public required string Href { get; init; }
 }

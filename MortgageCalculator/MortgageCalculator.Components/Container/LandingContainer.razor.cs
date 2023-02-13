1a1b91dc7668f05cbcaabcd6b41f7dee1aacd6f4
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2023-02-13 17:12:15 +0000

    Base layout for landing page

---
+++ MortgageCalculator/MortgageCalculator.Components/Container/LandingContainer.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MortgageCalculator.Components.Container;
+
+public partial class LandingContainer : ComponentBase
+{
+    [Parameter, EditorRequired]
+    public RenderFragment ChildContent { get; set; } = default!;
+}
